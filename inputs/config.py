b5cb5a60f0a2580a0fc113485b471b1a58f2d76c
MsLolita <oblikovuizapuc@gmail.com>
2024-02-22 11:20:54 +0200

    change default params in config.py

--- inputs/config.py
+++ inputs/config.py
@@ -6,7 +6,7 @@ NEEDED_TRADE_VOLUME = 0  # volume to trade, if 0 it will never stop
 MIN_BALANCE_TO_LEFT = 0  # min amount to left on the balance, if 0, it is traded until the balance is equal to 0.
 
 TRADE_AMOUNT = [0, 0]  # minimum and maximum amount to trade in USD, if 0 it will trade on FULL balance
-ALLOWED_ASSETS = ["BONK_USDC"]
+ALLOWED_ASSETS = ["SOL_USDC", "PYTH_USDC", "JTO_USDC", "HNT_USDC", "MOBILE_USDC", "BONK_USDC", "WIF_USDC", "JUP_USDC"]
 
 
 
