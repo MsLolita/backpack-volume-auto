bca6037c6a8222c4a2bccce307c8262fa2dd1d5a
MsLolita <oblikovuizapuc@gmail.com>
2024-02-22 11:20:43 +0200

    change default params in config.py

--- inputs/config.py
+++ inputs/config.py
@@ -1,12 +1,12 @@
 THREADS = 1  # Enter amount of threads
 DELAY_BETWEEN_TRADE = (1, 3)  # Execute delay between every trade (Buy -> Delay -> Sell -> Buy -> Delay ...)
-DELAY_BETWEEN_DEAL = (1, 3)  # Execute delay between full trade (Buy -> Sell -> Delay -> Buy -> Sell -> Delay ...)
+DELAY_BETWEEN_DEAL = (0, 0)  # Execute delay between full trade (Buy -> Sell -> Delay -> Buy -> Sell -> Delay ...)
 
 NEEDED_TRADE_VOLUME = 0  # volume to trade, if 0 it will never stop
 MIN_BALANCE_TO_LEFT = 0  # min amount to left on the balance, if 0, it is traded until the balance is equal to 0.
 
 TRADE_AMOUNT = [0, 0]  # minimum and maximum amount to trade in USD, if 0 it will trade on FULL balance
-ALLOWED_ASSETS = ["SOL_USDC", "PYTH_USDC", "JTO_USDC", "HNT_USDC", "MOBILE_USDC", "BONK_USDC", "WIF_USDC", "JUP_USDC"]
+ALLOWED_ASSETS = ["BONK_USDC"]
 
 
 
