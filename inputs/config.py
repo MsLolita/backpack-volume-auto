dbef14502a117a5d320cd0fa89e439e8d927f012
MsLolita <oblikovuizapuc@gmail.com>
2024-04-02 13:11:08 +0300

    rename usdt -> usdc

--- inputs/config.py
+++ inputs/config.py
@@ -8,7 +8,7 @@ NEEDED_TRADE_VOLUME = 0  # volume to trade, if 0 it will never stop
 MIN_BALANCE_TO_LEFT = 0  # min amount to left on the balance, if 0, it is traded until the balance is equal to 0.
 
 TRADE_AMOUNT = [0, 0]  # (works nearly from +-20%), minimum (5$) and maximum amount to trade in USD, if 0 it will trade on FULL balance
-ALLOWED_ASSETS = ["SOL_USDC", "PYTH_USDC", "JTO_USDC", "HNT_USDC", "MOBILE_USDC", "BONK_USDC", "WIF_USDC", "JUP_USDC", "RENDER_USDC", "WEN_USDC", "BTC_USDT"]
+ALLOWED_ASSETS = ["SOL_USDC", "PYTH_USDC", "JTO_USDC", "HNT_USDC", "MOBILE_USDC", "BONK_USDC", "WIF_USDC", "JUP_USDC", "RENDER_USDC", "WEN_USDC", "BTC_USDС"]
 
 # Volatility moment
 # DEPTH of limit order to trade as market order
